40eadbf4b748e411fe292ede675bed1df40ace9d
m1hn34 <49846896+m1hn34@users.noreply.github.com>
2023-01-28 16:54:42 +0200

    add new kata

---
+++ calculating_with_functions/calc.py
@@ -0,0 +1,38 @@
+'''
+This time we want to write calculations using functions and get the results. 
+Let's have a look at some examples:
+
+seven(times(five())) # must return 35
+four(plus(nine())) # must return 13
+eight(minus(three())) # must return 5
+six(divided_by(two())) # must return 3
+Requirements:
+
+There must be a function for each number from 0 ("zero") to 9 ("nine")
+There must be a function for each of the following mathematical operations: 
+plus, minus, times, divided_by
+
+Each calculation consist of exactly one operation and two numbers
+The most outer function represents the left operand, the most inner function 
+represents the right operand
+Division should be integer division. For example, this should return 2, not 
+2.666666...:
+eight(divided_by(three()))
+
+'''
+def zero(): return 0
+def one(): return 1
+def two(): return 2
+def three(): return 3
+def four(): return 4
+def five(): return 5
+def six(): return 6
+def seven(): return 7
+def eight(): return 8
+def nine(): return 9
+
+
+def plus():  # your code here
+def minus():  # your code here
+def times():  # your code here
+def divided_by():  # your code here
---
+++ calculating_with_functions/test_calc.py
@@ -0,0 +1,8 @@
+# PyTest script for calc.py
+
+'''
+test.assert_equals(seven(times(five())), 35)
+test.assert_equals(four(plus(nine())), 13)
+test.assert_equals(eight(minus(three())), 5)
+test.assert_equals(six(divided_by(two())), 3)
+'''
