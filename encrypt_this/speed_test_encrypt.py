88bfb92b4fa1ad3a7932d5058820eff9671044bd
m1hn34 <49846896+m1hn34@users.noreply.github.com>
2023-03-01 12:28:21 +0200

    add code to testing script

--- encrypt_this/speed_test_encrypt.py
+++ encrypt_this/speed_test_encrypt.py
@@ -5,7 +5,21 @@ import timeit
 import_module = "import random"
 
 test_code_1 = '''
-
+def encrypt_this(text):
+    text = text.split(' ')
+    encrypted = []
+    for word in text:
+        if len(word) < 3:
+            encrypted.append(
+                str(ord(word[0])) + word[1])
+            encrypted.append(" ")
+        else:
+            encrypted.append(
+                str(ord(word[0])) + word[-1] + word[2:-1:] + word[1]
+            )
+            encrypted.append(" ")
+    text = "".join(encrypted)
+    return text.strip()
 '''
 
 
