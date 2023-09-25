dab4c5251c988b0bbe44193a6d8568ea63add578
m1hn34 <49846896+m1hn34@users.noreply.github.com>
2023-09-25 16:19:12 +0300

    code format
    just a test

--- word_spinning.py/test_spin_words.py
+++ word_spinning.py/test_spin_words.py
@@ -5,7 +5,7 @@ from spin_words import spin_words
 
 
 def test_0():
-    assert(spin_words('Welcome') == 'emocleW')
-    assert(spin_words('Hey fellow warriors') == 'Hey wollef sroirraw')
-    assert(spin_words('This is a test') == 'This is a test')
-    assert(spin_words('This is another test') == 'This is rehtona test')
+    assert (spin_words('Welcome') == 'emocleW')
+    assert (spin_words('Hey fellow warriors') == 'Hey wollef sroirraw')
+    assert (spin_words('This is a test') == 'This is a test')
+    assert (spin_words('This is another test') == 'This is rehtona test')
